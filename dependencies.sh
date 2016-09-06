683ef70cce4242d1193f8f22f2ed54a1efa4d7df
Jonathan Lurie <lurie.jo@gmail.com>
2016-09-06 09:15:30 -0400

    add: the script to install dependencies

---
+++ dependencies.sh
@@ -0,0 +1,10 @@
+# dependencies are installed here
+cd js-lib/
+
+# cloning dependencies (dev versions)
+git clone https://github.com/jonathanlurie/VolumeNavigator.git
+git clone https://github.com/jonathanlurie/MincReaderJS.git
+
+# cleaning the .git folders of dependencies
+rm -rf VolumeNavigator/.git
+rm -rf MincReaderJS/.git

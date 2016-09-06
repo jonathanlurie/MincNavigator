f5f5c8d997499d9d3dfcc118a64bb4efcfeb23d4
Jonathan Lurie <lurie.jo@gmail.com>
2016-09-06 10:08:33 -0400

    upd: removing the links before fetching dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,10 @@
 # dependencies are installed here
 cd js-lib/
 
+# removing links if any
+rm js-lib/MincReaderJS
+rm js-lib/VolumeNavigator
+
 # cloning dependencies (dev versions)
 git clone https://github.com/jonathanlurie/VolumeNavigator.git
 git clone https://github.com/jonathanlurie/MincReaderJS.git
